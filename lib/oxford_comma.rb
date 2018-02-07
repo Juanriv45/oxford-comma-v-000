e565145156fa826d184e2bbf760985976dc763a9
Juan Rivera <juanriv45@gmail.com>
2018-02-07 18:04:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
 
-  array[array.size - 1] = "and " + array[array.size - 1]
+  if array.size >= 3 do 
+    array[array.size - 1] = "and " + array[array.size - 1]
+  end
 array.join(", ")
 end
