06f90d1e2d279d75076b072c4c8124e968ae9822
Juan Rivera <juanriv45@gmail.com>
2018-02-07 18:08:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,10 @@ def oxford_comma(array)
 
   if array.size >= 3 then
     array[array.size - 1] = "and " + array[array.size - 1]
+  elsif array.size = 2 then
+    array[1] = "and " + array[1]
+  end
+    
   end
 array.join(", ")
 end
