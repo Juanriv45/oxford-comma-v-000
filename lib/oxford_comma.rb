6df41eb5841a48d3039bcdc4846cdc7495044ecf
Juan Rivera <juanriv45@gmail.com>
2018-02-07 18:09:24 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ def oxford_comma(array)
 
   if array.size >= 3 then
     array[array.size - 1] = "and " + array[array.size - 1]
+    array.join(", ")
   elsif array.size = 2 then
     array[1] = "and " + array[1]
+    array.join(", ")
   end
-    
-  end
-array.join(", ")
+
 end
